e7117854fa95dae93ee778ddf8e17e129da7543c
imcarlosmsX <sandovalcarlos@uninorte.edu.co>
2022-11-16 16:39:24 -0500

    cascarón.

---
+++ Parking_Lot.py
@@ -0,0 +1,30 @@
+from abc import *
+
+class parking(ABC):
+    abstractclassmethod
+
+    def guardar_carro():
+        ...
+
+class Usuario:
+
+    def __init__(self, name) -> None:
+        name = self.name
+    
+    def regitro():
+        pass
+
+class Vehiculo:
+    def __init__(self, tipo, tiempo, tipo_tiempo) -> None:
+        self.tipo = tipo
+        self.tiempo = tiempo
+        self.tipo_tiempo = tipo_tiempo
+
+
+class Parqueadero:
+    def __init__(self, hora_inicio, hora_fin) -> None:
+        self.hora_inicio = hora_inicio
+        self.hora_fin = hora_fin
+    
+    def guardar_vehiculo(Vehiculo):
+        pass
